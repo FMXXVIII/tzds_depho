ff61d3b294feba5fc7f707b46da571c7b3d38408
Victor Hugo <victor_hugoff@hotmail.com.br>
2023-11-30 22:42:25 -0300

    HARD

--- fakepintrest/__init__.py
+++ fakepintrest/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
+
+#import os
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#CONFIGURAR A VARIAVEL "DATABASE_URL" NO AMBIENTE DENTRO DO RENDER => KEY: VARIAVEL, VALUE: LINK DO BANCO
+#POSTGRESQL: PIP INSTALL PSYCOPG2
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "090cbdb0590663e6"
 app.config["UPLOAD_FOLDER"] = "static\\fotos_posts"
 
